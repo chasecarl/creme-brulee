f72de4f2347fb28eea6789c33bc56ef293580d11
Andrey C <andreychas1@gmail.com>
2023-04-08 12:35:35 +0300

    Initial commit
    Setups package manager (straight), use-package and completion (vertico)

---
+++ init.el
@@ -0,0 +1,85 @@
+(eval-and-compile
+  ;; master seems to throw native-comp related errors on Emacs 29
+  (setq straight-repository-branch "develop")
+  ;; should be at the top as 'use-package is a macro https://emacs.stackexchange.com/a/70424
+  (setq straight-use-package-by-default t))
+
+
+;; bootstrapping code from https://github.com/radian-software/straight.el#getting-started
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+(defun cb--setup-use-package ()
+  "Configures use-package."
+  (straight-use-package 'use-package))
+
+
+(defun cb-package-management ()
+  "Configures package manager and package loading."
+  (cb--setup-use-package))
+
+
+(defun cb--setup-vertico ()
+  "Configures vertico."
+  (use-package vertico
+    :init
+    (vertico-mode)
+    (setq vertico-cycle t)
+    ;; Add prompt indicator to `completing-read-multiple'.
+    ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+    (defun crm-indicator (args)
+      (cons (format "[CRM%s] %s"
+		      (replace-regexp-in-string
+		       "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+		       crm-separator)
+		      (car args))
+	      (cdr args)))
+    (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+    ;; Do not allow the cursor in the minibuffer prompt
+    (setq minibuffer-prompt-properties
+	  '(read-only t cursor-intangible t face minibuffer-prompt))
+    (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+    ;; Enable recursive minibuffers
+    (setq enable-recursive-minibuffers t))
+
+  ;; Persist history over Emacs restarts. Vertico sorts by history position.
+  (use-package savehist
+    :init
+    (savehist-mode))
+
+  ;; Use the `orderless' completion style.
+  (use-package orderless
+    :init
+    ;; Configure a custom style dispatcher (see the Consult wiki)
+    ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+    ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+    (setq completion-styles '(orderless basic)
+	  completion-category-defaults nil
+	  completion-category-overrides '((file (styles partial-completion))))))
+
+
+(defun cb-completion ()
+  "Loads and configures completion backend."
+  (cb--setup-vertico))
+
+
+(defun cb-load-packages ()
+  "Loads all specified packages."
+  (cb-completion))
+
+
+(cb-package-management)
+(cb-load-packages)
