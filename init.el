e02e6bf84f675769b5cbc2720f0f77bcd0f56f65
Andrey C <andreychas1@gmail.com>
2023-04-08 16:36:26 +0300

    Move straight bootstrap inside eval-and-compile
    Not sure if it actually does something, but this way all top forms are
    grouped together

--- init.el
+++ init.el
@@ -1,23 +1,22 @@
 (eval-and-compile
   ;; master seems to throw native-comp related errors on Emacs 29
   (setq straight-repository-branch "develop")
-  ;; should be at the top as 'use-package is a macro https://emacs.stackexchange.com/a/70424
-  (setq straight-use-package-by-default t))
 
-
-;; bootstrapping code from https://github.com/radian-software/straight.el#getting-started
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  ;; all of the below should be at the top as 'use-package is a macro https://emacs.stackexchange.com/a/70424
+  (setq straight-use-package-by-default t)
+  ;; bootstrapping code from https://github.com/radian-software/straight.el#getting-started
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
 
 
 (defun cb--setup-use-package ()
