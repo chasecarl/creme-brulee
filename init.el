df5bf9826e51f0a0bffa72521f3ce41d5ca17963
Andrey C <andrey@deepsea.group>
2023-06-01 16:44:39 +0300

    Add window rotation

--- init.el
+++ init.el
@@ -108,7 +108,8 @@
      [("h" "Go to the window on the left" windmove-left)
       ("j" "Go to the window below" windmove-down)
       ("k" "Go to the window above" windmove-up)
-      ("l" "Go to the window on the right" windmove-right)]])
+      ("l" "Go to the window on the right" windmove-right)
+      ("r" "Rotate windows" cb-rotate-windows)]])
   ;; TODO: merge into one function (the if-version throws error when used
   ;;       on `cb-manage-windows')
   (defun cb--ensure-transient-hidden-popup (orig-fun &rest args)
@@ -131,6 +132,31 @@
     (dolist (func funcs)
       (advice-mapc (lambda (advice _props) (advice-remove func advice)) func))))
   ;; (cb-remove-advice-from-functions '(transient--redisplay cb-manage-windows)))
+
+
+(defun cb-rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the other direction.
+
+Taken from https://www.emacswiki.org/emacs/TransposeWindows ('Via Robert
+Bost's solution' section)."
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (prefix-numeric-value arg))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse 'identity)))
+      (dotimes (_ (abs rotate-times))
+        (dotimes (i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)))))))
 
 
 (defun cb-layout-management ()
