3e076f38b950f9d96d8583f86a6ed1a2c756cac9
Andrey Cherkasov <andreychas1@gmail.com>
2024-02-22 19:43:55 +0200

    Only show the current perspective name

--- init.el
+++ init.el
@@ -196,7 +196,9 @@ Bost's solution' section)."
     :init
     (setq persp-state-default-file
 	  (expand-file-name "persp-state.el" user-emacs-directory)
-	  persp-mode-prefix-key (kbd "C-x l"))
+	  persp-mode-prefix-key (kbd "C-x l")
+          persp-modestring-short 't
+          )
     (persp-mode)
     (when (file-exists-p persp-state-default-file)
       (persp-state-load persp-state-default-file))
