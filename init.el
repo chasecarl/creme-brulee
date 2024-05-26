76cb95a1d4deb5f8174b9ef2be1b60f76812bb76
Andrey Cherkasov <andreychas1@gmail.com>
2024-05-26 17:39:44 +0300

    Setup org-roam-ui

--- init.el
+++ init.el
@@ -462,6 +462,16 @@ modes, etc.
     (org-roam-setup)
     )
 
+  (use-package org-roam-ui
+    :straight
+    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
   (use-package ebib
     :config
     (add-to-list 'ebib-preload-bib-files
