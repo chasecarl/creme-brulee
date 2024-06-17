40c00741b862d376f5f2c473609e85c77e4684ea
Andrey Cherkasov <andreyc@mediaforce.com>
2024-06-17 15:34:56 +0300

    Refactor repetitions in cb-setup-shell-and-terminal
    Also includes vterm-mode to cb-no-line-number-modes.

--- init.el
+++ init.el
@@ -1014,9 +1014,11 @@ Unlike `org-get-heading', include the stars."
 
 (defun cb-setup-shell-and-terminal ()
   "Setups shell(s) and terminal emulator(s)."
-  (add-to-list 'cb-no-line-number-modes 'eshell-mode)
-  (add-to-list 'cb-no-line-number-modes 'term-mode)
   (use-package vterm)
+  (dolist (mode '(eshell-mode
+                  term-mode
+                  vterm-mode))
+    (add-to-list 'cb-no-line-number-modes mode))
   (add-hook 'term-exec-hook (lambda ()
 			      (set-buffer-process-coding-system 'utf-8-unix
 								'utf-8-unix)))
