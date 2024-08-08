eed4e2801f6c24e8acf2dbd20ea2b3f53cdf3ba6
Andrey Cherkasov <andreychas1@gmail.com>
2024-08-09 01:47:34 +0300

    Revert "Freeze python-pytest"
    This reverts commit b1029454c7a7d358fd25f3f51ba7f8c629b83fa2.
    
    The problem is in transient, and not in emacs-python-pytest.

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -1,5 +1,4 @@
 (
- ("emacs-python-pytest" . "33c921adaa6c9c8f7cceba2342114c6b406e0d7c")
  ("magit" . "f59df798194d4213b5a2b2f43a2ee25b71cbbb90")
  ("transient" . "0ae0de43590b5b6984a83f9e044e7c426455ac6e")
  )
